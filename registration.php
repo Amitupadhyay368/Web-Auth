47c04ea6124d58761582adcd956c2cfa7f0c28b9
Amitupadhyay368 <57106633+Amitupadhyay368@users.noreply.github.com>
2021-07-18 15:37:36 +0530

    Update registration.php

--- registration.php
+++ registration.php
@@ -59,8 +59,8 @@ else{
         $mail->isSMTP();                                            // Send using SMTP
         $mail->Host       = 'smtp.hostinger.in';                    // Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-        $mail->Username   = 'admin@elitesrank.com';                     // SMTP username
-        $mail->Password   = 'Technocrats@1234';                               // SMTP password
+        $mail->Username   = 'username';                     // SMTP username
+        $mail->Password   = 'password';                               // SMTP password
         $mail->SMTPSecure = 'tls';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
         $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
     
